6fb460ab315745a9751397c05da8364c2d43f1e7
Alexandre Jung <jungalexandre@ymail.com>
2021-06-25 09:35:21 +0200

    Slight adjustments
    - Change html language from 'en' to 'fr'
    - Remove a useless constructor
    - Remove unnecessary import in SignupForm.tsx
    - Adjust form illustration width on large screens

--- src/components/SignupForm.tsx
+++ src/components/SignupForm.tsx
@@ -1,4 +1,4 @@
-import React, { FormEvent, FormEventHandler } from "react";
+import React, { FormEvent } from "react";
 import Input from "./Input";
 import SubmitButton from "./SubmitButton";
 import FormIllustration from './FormIllustration';
@@ -13,14 +13,11 @@ export default class SignupForm extends React.Component<SignupFormProps> {
         title: '',
     }
 
-    constructor(props: SignupFormProps) {
-        super(props);
-    }
-
     protected handleSubmit = (event: FormEvent<HTMLFormElement>) => {
 
         // The form is being submitted
         // ...
+        alert('Le formulaire a été soumis');
         event.preventDefault();
     }
 
@@ -36,7 +33,7 @@ export default class SignupForm extends React.Component<SignupFormProps> {
                         <Input id='firstName' type='text' label='Prénom' compact />
                         <Input id='email' type='email' label='Email' required />
                         <Input id='password' type='password' label='Mot de passe' required compact />
-                        <Input id='password' type='passwordCheck' label='Vérifiez le mot de passe' required compact />
+                        <Input id='passwordCheck' type='password' label='Vérifiez le mot de passe' required compact />
                         <small className='text-black-50 mt-3'>Les champs marqués d'un * sont obligatoires</small>
                         <SubmitButton value='Inscription' />
                     </div>
